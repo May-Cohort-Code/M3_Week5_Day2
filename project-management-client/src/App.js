c2bfae6914feb8050ad375e38c0ffe905679d97f
omarakamal <54825038+omarakamal@users.noreply.github.com>
2023-09-28 23:47:04 +0400

    Add files via upload

---
+++ project-management-client/src/App.js
@@ -0,0 +1,40 @@
+ 
+import "./App.css";
+import { Routes, Route } from "react-router-dom"; // <== IMPORT
+ 
+import Navbar from "./components/Navbar";     // <== IMPORT
+import HomePage from "./pages/HomePage";     // <== IMPORT
+import ProjectsList from "./pages/ProjectsList";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
+import AddProject from "./pages/AddProject";
+import EditProject from "./pages/EditProject";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+ 
+/* 
+Objective: Full CRUD with my react application
+*/
+
+
+function App() {
+  return (
+    <div className="App">
+      
+     {/* Below: ADD <Navbar>, <Routes> & <Route> */}
+      <Navbar />
+ 
+      <Routes>      
+        <Route path="/" element={ <HomePage /> } />
+        <Route path='/projects' element={<ProjectsList/>}/>
+        <Route path="/projects/:id" element={<ProjectDetailsPage></ProjectDetailsPage>}></Route>
+        {/* <Route path="/projects/add" element={<AddProject></AddProject>}></Route> */}
+        <Route path="/projects/:id/edit" element={<EditProject></EditProject>}></Route>
+        <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+      
+    </div>
+  );
+}
+ 
+export default App;
